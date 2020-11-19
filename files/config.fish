e120daae8b516eff60ac0ccf2ac9cd5055179367
f3yzee <fzl.32x@gmail.com>
2020-11-19 17:25:50 +0530

    fixes, rearranged packages and fish completions

--- files/config.fish
+++ files/config.fish
@@ -1,17 +1,29 @@
-set -gx GOPATH $HOME/go
+set -Ux GOPATH ~/.go
+set -Ux QT_QPA_PLATFORMTHEME gnome
+set -Ux QT_STYLE_OVERRIDE adwaita-dark
+set -Ux QT_QPA_PLATFORM wayland
+set -Ux EDITOR /usr/bin/vim
+set -Ux FZF_DEFAULT_COMMAND 'fd --type file --follow --color=always --hidden --exclude .git'
+set -Ux FZF_FIND_FILE_COMMAND 'fd --type file --folloe --color=always --hidden --exclude=.git . \$dir'
+set -Ux FZF_PREVIEW_DIR_CMD 'ls'
+set -Ux FZF_DEFAULT_OPTS '-m --height=70% --preview="bat --theme 'Dracula' {}" --preview-window=right:60%:wrap'
 
 starship init fish | source
 
 alias python "python3"
+alias protontricks "flatpak run --command=protontricks com.valvesoftware.Steam --no-runtime"
+alias cp "cp -pur"
 
-abbr sd "sudo dnf install -y"
+abbr s "sudo"
+abbr sd "sudo dnf install"
+abbr dnfs "sudo dnf search"
 abbr update "sudo dnf update"
-abbr v "vim"
+abbr remove "sudo dnf remove"
 abbr gcom "git commit -m"
 abbr gc "git clone"
 abbr gch "git checkout"
 abbr gm "git merge"
-abbr ga "git add"
+abbr gadd "git add"
 abbr gpo "git push origin"
 abbr gpom "git push origin master"
 abbr gst "git status"
@@ -24,9 +36,9 @@ abbr tfr "terraform refresh"
 abbr dk "docker"
 abbr dkr "docker run"
 abbr dkb "docker build"
-abbr dks "docker start"
-abbr dkst "docker stop"
-abbr dkp "docker pull"
+abbr dkst "docker start"
+abbr dkstop "docker stop"
+abbr dkpull "docker pull"
 abbr dkps "docker ps"
 abbr dki "docker images"
 abbr dkc "docker-compose"
@@ -39,11 +51,10 @@ abbr xcs "xclip -sel clip"
 abbr ssc "sudo systemctl"
 abbr ssce "sudo systemctl enable"
 abbr sscs "sudo systemctl start"
-abbr finds "find ~ -name"
-abbr findfile "find -type f ~ -name"
-abbr finddir "find -type d ~ -name"
-abbr findpwd "find . -name"
-abbr findroot "find / -name"
+abbr fd "fd -Hi" # hidden + ignore case
+abbr fde "fd -e" # extension of file
+abbr fds "find -S" # size based search
+abbr fdd "fd -t d" # directory only
 abbr py "python"
 abbr py2 "python2"
 abbr wcp "wl-copy"
@@ -53,4 +64,38 @@ abbr vgup "vagrant up"
 abbr vgh "vagrant halt"
 abbr vgd "vagrant destroy -f"
 abbr vgssh "vagrant ssh"
-abbr py "python"
+#abbr b64d "echo '' | base64 --decode"
+#abbr b64e "echo '' | base64 --encode"
+abbr pyserver "python -m http.server"
+abbr kssh "kitty +kitten ssh"
+
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
